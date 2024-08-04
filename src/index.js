945a99f634b7dc2878e59af1704b5752a7b79068
Het Mardiya <hetmardiya9@gmail.com>
2024-08-05 01:27:11 +0530

    update a app.js file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import 'dotenv/config'
+import { app } from './app.js'
 import DB_Connection from "./db/connection.js"
+let port = process.env.PORT || 4000
 
 DB_Connection()
+.then(()=>{
+    app.listen(port , ()=>{
+        console.log(`app is listing to port no ${port}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log(`error from index.js file in DB_Connection method ${err}`);
+    
+})
