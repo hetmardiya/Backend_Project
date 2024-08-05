14ba0024793a1511527295fa0c3b3ffae0cc3a41
Het Mardiya <hetmardiya9@gmail.com>
2024-08-05 18:31:43 +0530

    adding another content

--- src/app.js
+++ src/app.js
@@ -1,4 +1,16 @@
 import express from "express";
 const app = express()
+import cookieParser from "cookie-parser";
+import cors from "cors"
 
+// cors is for use the middlewares and other configuration
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,               //THIS TWO LINES ARE NOT COMPALSORY BUT USED FOR CONFIG THE ORIGIN AND OTHER THINGS
+    Credential : true
+}))
+                                                                 //THESE FOUR LINE ARE BASICALLY FOR CONFIGURE THE THINGS WHICH IS USE IN PROJECT
+app.use(express.json({limit:"16kb"}))                            //comes input in json format so it is for that with 16kb limit
+app.use(express.urlencoded({limit:"16kb", extends:true}))        //comes input in url format so it is for that with 16kb limit
+app.use(express.static("public"))                                //the static path of public folder when any thing you want to store in so
+app.use(cookieParser())                                          //this is for configure cookie
 export { app }
---
+++ src/utils/API_Response.js
@@ -0,0 +1,10 @@
+class Api_Response {
+    constructor(statusCode,message="success",data) {
+        this.statusCode = statusCode
+        this.message = message
+        this.data = data
+        this.success = statusCode < 400
+    }
+}
+
+export {Api_Response}
